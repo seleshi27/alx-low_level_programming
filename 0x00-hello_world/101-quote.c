6aca37e7a07e2c56f9a37ab09a6a4871d45a0fe1
seleshi27 <110991479+seleshi27@users.noreply.github.com>
2022-12-12 15:44:06 +0300

    Create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*main - Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+*
+* Return: Always 0
+*/
+int main(void)
+{
+  write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n")
+  return 1;
+}

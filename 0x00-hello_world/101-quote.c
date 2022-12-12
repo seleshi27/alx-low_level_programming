b0850df3e0babb17789339baf7202493d0cf8da9
seleshi27 <110991479+seleshi27@users.noreply.github.com>
2022-12-12 15:50:52 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
-*main - Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
-*
-* Return: Always 0
-*/
+ * main - Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ * Return: 1 if success.
+ */
 int main(void)
 {
-  fwrite(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n")
-  return 1;
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }

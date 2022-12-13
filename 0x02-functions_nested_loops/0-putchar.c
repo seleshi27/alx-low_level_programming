795aec7c23ceb142bec2f09cbffe7125bc72e72e
seleshi27 <110991479+seleshi27@users.noreply.github.com>
2022-12-13 16:01:02 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,8 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int _putchar()
 {
-	return (write(1, &c, 1));
+	putchar("_putchar \n");
+	return (0);
 }

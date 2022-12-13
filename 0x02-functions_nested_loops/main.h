58f558cdf6bf91ae0a8ef1aee07360dccf848f4a
seleshi27 <110991479+seleshi27@users.noreply.github.com>
2022-12-13 15:49:16 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,5 @@
 #include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
+int main()
+    {
+int _putchar(char c);
+    }

5dc350908de533b16d05e4d52cade55f32f0d246
seleshi27 <110991479+seleshi27@users.noreply.github.com>
2022-12-23 10:21:21 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,50 +1,25 @@
- #include "main.h"
- /**
-  * print_number - print integer with putchar
+#include "main.h"
+
+/**
+  * print_number - Prints any integer with putchar
+  * @n: Number to prints
   *
-  * @n: function print integer n
-  *
-  * Return: none
+  * Return: Nothing
   */
- void print_number(int n)
- {
-     int i;
-     int j;
-     int k;
-     unsigned int l;
-     unsigned int m;
- 
-     j = 1;
-     k = 0;
-     l = n;
-     m = n;
-     if (n == 0)
-     {
-         _putchar(48);
-     }
-     if (n < 0)
-     {
-         _putchar(45);
-         n = n + 1;
-         n = -n;
-         m = n;
-         l = n;
-         l = l + 1;
-         m = m + 1;
-     }
-     while (l != 0)
-     {
-         l = (l / 10);
-         k++;
-     }
-     for (i = 1; i < k; i++)
-     {
-         j = (j * 10);
-     }
-     for (i = 0; i < k; i++)
-     {
-         _putchar((m / j) + 48);
-         m = (m % j);
-         j = (j / 10);
-     }
- }
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}

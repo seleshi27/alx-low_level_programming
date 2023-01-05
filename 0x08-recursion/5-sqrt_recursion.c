4e9a3181f1daddfa9f45261b6f20f8a0099a0721
seleshi27 <110991479+seleshi27@users.noreply.github.com>
2023-01-05 03:10:27 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,27 +1,37 @@
 #include "main.h"
+
 /**
- * _sqrt_recursion -  returns the natural square root of a number.
- * @n: integer
- * Return: squareroot or -1
+ *is_sqrt - checks if number is square root of a number
+ *
+ *@g: number to check if it is a square root
+ *@r: test number
+ *
+ *Return: square root g, -1 if else
  */
+
+int is_sqrt(int g, int r)
+{
+	if (g * g == r)
+		return (g);
+	if (g * g < r)
+		return (is_sqrt(++g, r));
+	else
+		return (-1);
+}
+
+
+/**
+ *_sqrt_recursion - returns the natural square root of a number
+ *
+ *@n: numbert to test
+ *
+ *Return: square root of number n
+ */
+
 int _sqrt_recursion(int n)
 {
-if (n == 0)
-return (0);
-return (check(1, n));
-}
-/**
- * check - checks for the square root
- * @a: integer
- * @b: integer
- * Return: int
- */
-int check(int a, int b)
-{
-if (a * a > b)
-return (-1);
-else if (a * a == b)
-return (a);
-else
-return (check(a + 1, b));
+	if (n != 0)
+	return (is_sqrt(0, n));
+	else
+		return (0);
 }
